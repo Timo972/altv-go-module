6a789785d5414bf594695a8c87dd1a62683c122d
shock <fastestmanonmars@gmail.com>
2021-07-31 20:15:30 +0200

    first commit

---
+++ src/Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Davide on 31/07/2021.
+//
+
+#include "Main.h"
---
+++ src/Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Davide on 31/07/2021.
+//
+
+#ifndef ALTV_GO_MODULE_MAIN_H
+#define ALTV_GO_MODULE_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //ALTV_GO_MODULE_MAIN_H
