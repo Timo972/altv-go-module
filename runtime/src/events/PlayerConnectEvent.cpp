43b97902820b50c7be5edfd5eee5a32d065ba169
shock <fastestmanonmars@gmail.com>
2021-08-06 03:55:07 +0200

    Fix PlayerConnectEvent

--- runtime/src/events/PlayerConnectEvent.cpp
+++ runtime/src/events/PlayerConnectEvent.cpp
@@ -1,8 +1,7 @@
 #include "PlayerConnectEvent.h"
 
 void Go::PlayerConnectEvent::Call(const alt::CEvent *ev, const std::string& resourceName, ModuleLibrary* module) {
-    auto call = GET_FUNC(module, "altPlayerConnectEvent", void(*)(const std::string& resource,
-            alt::IPlayer* playerObject));
+    auto call = GET_FUNC(module, "altPlayerConnectEvent", void(*)(alt::IPlayer* playerObject));
     if(call == nullptr) {
         alt::ICore::Instance().LogError("Couldn't not call PlayerConnectEvent");
         return;
@@ -11,5 +10,5 @@ void Go::PlayerConnectEvent::Call(const alt::CEvent *ev, const std::string& reso
     auto event = dynamic_cast<const alt::CPlayerConnectEvent*>(ev);
     auto player = event->GetTarget().Get();
 
-    call(resourceName, player);
+    call(player);
 }
