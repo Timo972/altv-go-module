878e5dfa1495a47c722dbf05c01c61efda341963
shock <fastestmanonmars@gmail.com>
2021-08-06 23:41:50 +0200

    Fix Linux

--- runtime/src/Library.h
+++ runtime/src/Library.h
@@ -1,20 +1,15 @@
 #pragma once
 
-#ifdef _WIN32
-
+#ifndef IS_UNIX
 #include "windows.h"
-
 #define SEPARATOR "\\"
 #define LOAD_LIB(path) LoadLibrary(path)
 #define GET_FUNC(module, name, type) (type)GetProcAddress(module, name);
-
 using ModuleLibrary = HINSTANCE__;
 #else
 #include <dlfcn.h>
-
 #define SEPARATOR "/"
 #define LOAD_LIB(name) dlopen(name, RTLD_NOW);
 #define GET_FUNC(module, name, type) (type)dlsym(module, name);
-
 #endif
 
