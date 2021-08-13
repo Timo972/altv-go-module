d2d4c7bd3b676f35a44dda17833cf53db7c334ef
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-08-13 19:00:27 +0200

    add metadata support for generator

--- tools/gen-body-from-def.js
+++ tools/gen-body-from-def.js
@@ -18,7 +18,39 @@ const reinterprets = {
   Vehicle: "alt::IVehicle*",
 };
 
-const returnsWithImplicitOrNoCast = ["long", "bool", "float", "unsigned long"];
+const returnsWithImplicitOrNoCast = [
+  "long",
+  "bool",
+  "float",
+  "unsigned long",
+  "int",
+];
+
+const defaultGetMeta = (varName, cast, base, args) =>
+  `
+    auto ${varName} = reinterpret_cast<${cast}>(${base.name});
+    auto meta = ${varName}->GetMetaData(${args[0].name});
+
+    // Temporary
+    MetaData metaData;
+    metaData.Ptr = meta.Get();
+    metaData.Type = static_cast<unsigned char>(meta->GetType());
+
+    return metaData;
+`;
+const defaultSetMeta = (varName, cast, base, args) =>
+  `
+    auto ${varName} = reinterpret_cast<${cast}>(${base.name});
+    auto value = reinterpret_cast<alt::IMValue*>(${args[1].name});
+
+    ${varName}->SetMetaData(${args[0].name}, value);
+`;
+const defaultDeleteMeta = (varName, cast, base, args) =>
+  `
+    auto ${varName} = reinterpret_cast<${cast}>(${base.name});
+    ${varName}->DeleteMetaData(${args[0].name});
+    ${varName}->RemoveRef();
+`;
 
 const defaultCast = (varName, cast, base) =>
   `auto ${varName} = reinterpret_cast<${cast}>(${base.name});`;
@@ -30,7 +62,18 @@ const voidRefFunc = (varName, funcName, args) =>
   `${varName}->${funcName}(${args.map((x) => x.name).join(", ")}).Get();`;
 
 const emptyTemplate = (definition) => `${definition}\n{\n\n}\n`;
-const fullTemplate = (definition, body) => `${definition}\n${body}`
+const fullTemplate = (definition, body) => `${definition}\n${body}`;
+
+function validateSetMetaDefinition(args) {
+  if (args[0].type.replace(/\s/g, "") !== "constchar*") return false;
+  if (args[1].type.replace(/\s/g, "") !== "void*") return false;
+  return true;
+}
+
+function validateGetAndDeleteMetaDefinition(args) {
+  if (args[0].type.replace(/\s/g, "") !== "constchar*") return false;
+  return true;
+}
 
 function generateCAPIFunction(match, className, funcName, returnType, argStr) {
   const definition = match.substring(0, match.length - 1);
@@ -53,30 +96,47 @@ function generateCAPIFunction(match, className, funcName, returnType, argStr) {
   const base = args.shift();
 
   let valid = true;
+  let isMeta = true;
   let funcBody = `{\n`;
-
-  if (funcName.includes("Meta")) return [emptyTemplate(definition), false];
 
   if (typeof castTo == "undefined")
     throw new Error(`Could not find cast for class ${className}`);
 
-  funcBody += `   ${defaultCast(mainVarName, castTo, base)}\n`;
-
-  if (returnsWithImplicitOrNoCast.includes(trimmedReturnType)) {
-    // console.log(`Processing easy getter: ${className}_${funcName}`);
-    funcBody += `   return ${defaultFunc(mainVarName, funcName, args)}\n`;
-  } else if (trimmedReturnType === "void") {
-    // console.log(`Processing setter: ${className}_${funcName}`);
-    funcBody += `   ${defaultFunc(mainVarName, funcName, args)}\n`;
-  } else if (trimmedReturnType === "const char *") {
-    // console.log(`Processing c_str getter: ${className}_${funcName}`);
-    funcBody += `   return ${cStrFunc(mainVarName, funcName, args)}\n`;
-  } else if (trimmedReturnType.replace(" ", "") === "void*") {
-    // console.log(`Processing void* getter: ${className}_${funcName}`);
-    funcBody += `   return ${voidRefFunc(mainVarName, funcName, args)}\n`;
+  if (funcName.includes("Set") && funcName.includes("MetaData")) {
+    if (validateSetMetaDefinition(args)) {
+      funcBody += defaultSetMeta(mainVarName, castTo, base, args);
+    } else return [emptyTemplate(definition), false];
+  } else if (funcName.includes("Get") && funcName.includes("MetaData")) {
+    if (validateGetAndDeleteMetaDefinition(args)) {
+      funcBody += defaultGetMeta(mainVarName, castTo, base, args);
+    } else return [emptyTemplate(definition), false];
+  } else if (funcName.includes("Delete") && funcName.includes("MetaData")) {
+    if (validateGetAndDeleteMetaDefinition(args)) {
+      funcBody += defaultDeleteMeta(mainVarName, castTo, base, args);
+    } else return [emptyTemplate(definition), false];
   } else {
-    //funcBody += defaultCast(mainVarName, castTo, base);
-    valid = false;
+    isMeta = false;
+  }
+
+  if (!isMeta) {
+    funcBody += `   ${defaultCast(mainVarName, castTo, base)}\n`;
+
+    if (returnsWithImplicitOrNoCast.includes(trimmedReturnType)) {
+      // console.log(`Processing easy getter: ${className}_${funcName}`);
+      funcBody += `   return ${defaultFunc(mainVarName, funcName, args)}\n`;
+    } else if (trimmedReturnType === "void") {
+      // console.log(`Processing setter: ${className}_${funcName}`);
+      funcBody += `   ${defaultFunc(mainVarName, funcName, args)}\n`;
+    } else if (trimmedReturnType === "const char *") {
+      // console.log(`Processing c_str getter: ${className}_${funcName}`);
+      funcBody += `   return ${cStrFunc(mainVarName, funcName, args)}\n`;
+    } else if (trimmedReturnType.replace(" ", "") === "void*") {
+      // console.log(`Processing void* getter: ${className}_${funcName}`);
+      funcBody += `   return ${voidRefFunc(mainVarName, funcName, args)}\n`;
+    } else {
+      //funcBody += defaultCast(mainVarName, castTo, base);
+      valid = false;
+    }
   }
 
   funcBody += `}\n\n`;
