25e5a2c281818fabe51feb1ff4c199bb28d5da49
Timo972 <beckmann.timo@icloud.com>
2021-08-15 15:01:53 +0200

    add colshape capi methods

---
+++ runtime/src/capi/ColShape.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include "Main.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    // BaseObject inherited
+    EXPORT int ColShape_GetType(void *c);
+    EXPORT int ColShape_HasMetaData(void* base, const char *key);
+    EXPORT MetaData ColShape_GetMetaData(void* base, const char *key);
+    EXPORT void ColShape_SetMetaData(void *base, const char *key, void *val);
+    EXPORT void ColShape_DeleteMetaData(void *base, const char *key);
+
+    // WorldObject inherited
+    EXPORT Position ColShape_GetPosition(void *p);
+    EXPORT void ColShape_SetPosition(void* p, float x, float y, float z);
+    EXPORT long ColShape_GetDimension(void* p);
+    EXPORT void ColShape_SetDimension(void* p, long dimension);
+
+    // ColShape inherited
+    EXPORT int ColShape_GetColShapeType(void *c);
+    EXPORT int ColShape_IsEntityIn(void *c, void *e);
+    EXPORT int ColShape_IsPointIn(void *c, float x, float y, float z);
+    EXPORT void ColShape_SetPlayersOnly(void *c, int state);
+    EXPORT int ColShape_IsPlayersOnly(void *c);
+
+#ifdef __cplusplus
+}
+#endif
